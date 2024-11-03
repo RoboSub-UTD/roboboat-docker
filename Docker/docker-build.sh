22e57b576351e7291f202db74eb5e46d2853e13a
samminhch@minhtop <45446622+samminhch@users.noreply.github.com>
2024-11-03 17:43:24 -0600

    removed un-needed docker-build/run.sh

--- Docker/docker-build.sh
+++
@@ -1,4 +0,0 @@
-IMAGE_NAME="roboboat"
-IMAGE_TAG="devel"
-DOCKERFILE_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-docker build ${DOCKERFILE_DIR} -t "${IMAGE_NAME}:${IMAGE_TAG}" -f "${DOCKERFILE_DIR}/Dockerfile"
